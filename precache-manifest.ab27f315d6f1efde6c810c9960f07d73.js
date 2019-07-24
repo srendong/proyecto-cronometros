301a655253afc8e0f4b6260e653c4b20871bf0c9
Samuel Rendon G <s.redon.g@gmail.com>
2019-07-24 17:55:52 -0500

    Updates

---
+++ precache-manifest.ab27f315d6f1efde6c810c9960f07d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7fdb9d61450a7648d34db4aade6226",
+    "url": "/proyecto-cronometros/index.html"
+  },
+  {
+    "revision": "e53f6f2b3b707d8687dd",
+    "url": "/proyecto-cronometros/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "31bb6bea7865306a3487",
+    "url": "/proyecto-cronometros/static/css/main.3f91f624.chunk.css"
+  },
+  {
+    "revision": "e53f6f2b3b707d8687dd",
+    "url": "/proyecto-cronometros/static/js/2.95524e29.chunk.js"
+  },
+  {
+    "revision": "31bb6bea7865306a3487",
+    "url": "/proyecto-cronometros/static/js/main.3eb5ce7d.chunk.js"
+  },
+  {
+    "revision": "626122f7478f25a613e7",
+    "url": "/proyecto-cronometros/static/js/runtime~main.737f6509.js"
+  }
+]);
